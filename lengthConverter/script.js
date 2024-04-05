88f11c8207f889b40600eb9e5a129c06aa7fba41
Ramanathan20032 <154827095+Ramanathan20032@users.noreply.github.com>
2024-04-05 22:58:35 +0530

    Delete lengthConverter directory

--- lengthConverter/script.js
+++
@@ -1,6 +0,0 @@
-function convert(){
-    let cmVal = Number(document.getElementById("input").value)
-    let inchVal = cmVal / 2.54
-    let result = document.getElementById("result")
-    result.textContent = inchVal.toFixed(3) + " inches"
-}
